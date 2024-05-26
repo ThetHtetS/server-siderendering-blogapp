c3fa52158d8c9a4ac98cf48cc9de7dd166e94026
thet htet <thethtetsoe114@gmail.com>
2024-05-26 10:45:27 +0630

    fix minor error

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -6022,7 +6022,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34417" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46467" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
