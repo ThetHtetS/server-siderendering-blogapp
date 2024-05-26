b27e7ba19aa313590c8d7c91cda7fae68d2be2a0
thet htet <thethtetsoe114@gmail.com>
2024-05-26 11:15:15 +0630

    add image store directory

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -6024,7 +6024,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38615" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42071" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
