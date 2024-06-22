e466407d1b257808c8c9cd5b49b0bffc67fe048a
thet htet <thethtetsoe114@gmail.com>
2024-06-22 10:20:44 +0630

    fix

--- public/js/login.js
+++ public/js/login.js
@@ -4,17 +4,15 @@ import { showAlert } from './alerts';
 
 export const signup = async (name, email, password, passwordConfirm) => {
   try {
-   
        const res = await axios({
-      method: 'POST',
-      url: '/api/v1/users/signup',
-      // url: ,
-      data: {
-        name,
-        email,
-        password,
-        passwordConfirm
-      }
+        method: 'POST',
+        url: '/api/v1/users/signup',
+        data: {
+          name,
+          email,
+          password,
+          passwordConfirm
+       }
     });
 
     if (res.data.status === 'success') {
